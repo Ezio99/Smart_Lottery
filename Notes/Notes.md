db6b21697c8ad5d01f9e255e6b1e842361a61f0c
Vaibhav <vaibhavritam@gmail.com>
2025-08-21 13:48:41 +0800

    Added automation functions

--- Notes/Notes.md
+++ Notes/Notes.md
@@ -6,3 +6,29 @@ CHain link brownie installation guide and repo - https://github.com/smartcontrac
 
 The raffles contract is inheriting VRFConsumerBaseV2Plus whose constructor requires an address for vrfCoordinator , so we provide that in the following manner
 `constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator) `
+
+`
+       VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+`
+
+Here we craft a request to be sent to the VRF coordinator
+
+`uint256 requestId = s_vrfCoordinator.requestRandomWords(request);`
+On sending it we get a request id to track our request
+
+The VRFCooridinator then hits our contract's `rawFulfillRandomWords()` (inherited from VRFConsumerBaseV2Plus) which then calls our overrided `fulfillRandomWords` function to perform any action with the returned random word (number).
+
+
+Coding pattern : CEI (Checks, Effects (State changes), Interactions (External contract interactions)) - Try following this structure in your functions
+
+Also a best practice would be to emit events before interactions and after effects
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -17,6 +17,11 @@ contract Raffle is VRFConsumerBaseV2Plus {
     error Raffle__NotEnoughTimePassedToPickWinner();
     error Raffle__TransferToWinnerFailed();
     error Raffle__NotOpen();
+    error Raffle__UpKeepNotNeeded(
+        uint256 currentBalance,
+        uint256 numPlayers,
+        RaffleState raffleState
+    );
 
     /*Type Declarations */
     enum RaffleState {
@@ -83,15 +88,50 @@ contract Raffle is VRFConsumerBaseV2Plus {
         emit RaffleEntered(msg.sender);
     }
 
-    //1. Get a random number
-    //2. Use that random number to pick a winner
-    //3. Be automatically called after a certain time period
-    function pickWinner() external {
-        //Chec if enough time has passed
-        if (block.timestamp - s_lastTimeStamp < i_interval) {
-            revert Raffle__NotEnoughTimePassedToPickWinner();
-        }
+    // Chainlink Automation functions
+    //When should winner be picked?
+    /**
+     *
+     * @dev This function is called by the Chainlink Automation Node to check if upkeep is needed (time to pick winner).
+     * The following conditions must be met:
+     * 1. Enough time has passed since the last winner was picked.
+     * 2. The raffle must be in the OPEN state.
+     * 3. The contract has ETH. (has players)
+     * 4. Implicitly, the subscription must be funded with LINK.
+     * @return upkeepNeeded
+     * @return
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) public view returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool timeHasPassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
+        bool isOpen = (s_raffleState == RaffleState.OPEN);
+        bool hasBalance = (address(this).balance > 0);
+        bool hasPlayers = (s_players.length > 0);
+        upkeepNeeded = (timeHasPassed && isOpen && hasBalance && hasPlayers);
 
+        return (upkeepNeeded, "");
+    }
+
+    //1. Get a random number  -Done
+    //2. Use that random number to pick a winner -Done
+    //3. Be automatically called after a certain time period - Use chain link automation (chain link keepers)
+    function performUpkeep(bytes calldata /* performData */) external {
+        //Checks
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpKeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                s_raffleState
+            );
+        }
+        // //Check if enough time has passed
+        // if (block.timestamp - s_lastTimeStamp < i_interval) {
+        //     revert Raffle__NotEnoughTimePassedToPickWinner();
+        // }
+
+        //Effects
         s_raffleState = RaffleState.CALCULATING_WINNER;
 
         //Get Random Number
@@ -99,6 +139,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
         //We will use ChainLink VRF to get a random
         // 1. Request RNG
 
+        //Interactions
         VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
             .RandomWordsRequest({
                 keyHash: i_keyHash,
@@ -112,30 +153,33 @@ contract Raffle is VRFConsumerBaseV2Plus {
             });
 
         //s_vrfCoordinator is inherited from VRFConsumerBaseV2Plus
-        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+        //Returns a requestId but we don't need it here
+        s_vrfCoordinator.requestRandomWords(request);
 
         // 2. Get RNG
     }
 
     //Internal because the external contract calls raw fulfillRandomWords which then calls this
     function fulfillRandomWords(
-        uint256 requestId,
+        uint256 /*requestId*/,
         uint256[] calldata randomWords
     ) internal override {
+        //Effects
         uint256 winnerIndex = randomWords[0] % s_players.length;
         s_recentWinner = s_players[winnerIndex];
 
         s_raffleState = RaffleState.OPEN;
         s_players = new address payable[](0);
+        emit WinnerPicked(s_recentWinner);
 
+        //Interactions
         //Transfer the balance to the winner
-        (bool success, ) = s_recentWinner.call{value: address(this).balance}("");
+        (bool success, ) = s_recentWinner.call{value: address(this).balance}(
+            ""
+        );
         if (!success) {
             revert Raffle__TransferToWinnerFailed();
         }
-
-        emit WinnerPicked(s_recentWinner);
-
     }
 
     /** Getter funcs */
