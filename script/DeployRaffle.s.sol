c3fd717229aa6c580f9ed4128850b4de66889ff5
Vaibhav <vaibhavritam@gmail.com>
2025-08-22 14:12:22 +0800

    Added consumer to deploy raffle contract script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
-import {CreateVRFSubscription} from "script/Interactions.s.sol";
+import {CreateVRFSubscription, FundVRFSubscription, AddVRFConsumer} from "script/Interactions.s.sol";
 
 contract DeployRaffle is Script {
     // function run() public {
@@ -15,11 +15,20 @@ contract DeployRaffle is Script {
         HelperConfig.NetworkConfig memory networkConfig = helperConfig
             .getNetworkConfigByChainId();
 
+        //Create subscription if it doesn't exist
         if (networkConfig.subscriptionId == 0) {
-             CreateVRFSubscription createSubscription = new CreateVRFSubscription();
-             networkConfig.subscriptionId = createSubscription.createVRFSubscription(
-                networkConfig.vrfCoordinator);
+            CreateVRFSubscription createSubscription = new CreateVRFSubscription();
+            networkConfig.subscriptionId = createSubscription
+                .createVRFSubscription(networkConfig.vrfCoordinator);
         }
+
+        //Fund subscription
+        FundVRFSubscription fundSubscription = new FundVRFSubscription();
+        fundSubscription.fundVRFSubscription(
+            networkConfig.vrfCoordinator,
+            networkConfig.subscriptionId,
+            networkConfig.link
+        );
 
         vm.startBroadcast();
         Raffle raffle = new Raffle(
@@ -31,6 +40,15 @@ contract DeployRaffle is Script {
             networkConfig.callbackGasLimit
         );
         vm.stopBroadcast();
+
+        //Add consumer to subscription (Latest deployed raffle contract)
+        //Dont need to broadcast as broadcast is in addVRFConsumer
+        AddVRFConsumer addConsumer = new AddVRFConsumer();
+        addConsumer.addVRFConsumer(
+            address(raffle),
+            networkConfig.vrfCoordinator,
+            networkConfig.subscriptionId
+        );
 
         return (raffle, helperConfig);
     }
